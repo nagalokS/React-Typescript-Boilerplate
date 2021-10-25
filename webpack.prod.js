668f9fef424b060d3fafde961e15ef1463ca78ee
Naga <lol.vit9@gmail.com>
2021-10-26 01:21:32 +0300

    add bundle analaizer

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,12 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const baseConfig = require("./webpack.config.js");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 module.exports = merge(baseConfig, {
   target: "browserslist",
   mode: "production",
-  devtool: "cheap-module-source-map",
+  // devtool: "cheap-module-source-map",
   output: {
     path: path.resolve(__dirname, "build"),
     assetModuleFilename: "images/[hash][ext][query]",
@@ -13,5 +15,10 @@ module.exports = merge(baseConfig, {
   devServer: {
     hot: false,
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: process.env.ANAL || "disabled",
+    }),
+  ],
 });
