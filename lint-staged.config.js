daac8e28ebf93be835c8fc82e2f843a14a9a3e5e
Naga <lol.vit9@gmail.com>
2022-11-27 20:48:04 +0300

    put lint staged config together

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
+  '*.json': ['prettier --write --parser json'],
+  '*.md': ['prettier --write --parser markdown'],
+  '*.{ts,tsx}': ['prettier --write', 'eslint --fix'],
 };
